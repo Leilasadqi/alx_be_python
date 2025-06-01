afd0d3c26c74d7e78eed46caab2ccc340082b161
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-06-01 23:22:53 +0100

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -8,10 +8,9 @@ match priority:
     case "medium":
         reminder = f"Reminder: '{task}' is a medium priority task"
     case "low":
-        reminder = f"Note: '{task}' is a low priority task"
+        reminder = f"Reminder: '{task}' is a low priority task"
     case _:
-        reminder = f"Note: '{task}' has an unspecified priority"
-
+        reminder = f"Reminder: '{task}' has an unspecified priority"
 
 if time_bound == "yes":
     if priority in ["high", "medium"]:
